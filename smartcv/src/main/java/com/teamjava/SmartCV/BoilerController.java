31f2b2bb245bbb2dd6ac919eef99c0f3a6cbbcd7
ricco1310cgi <55280954+ricco1310cgi@users.noreply.github.com>
2019-09-19 16:32:18 +0200

    Created BoilerReader to fetch from server

--- smartcv/src/main/java/com/teamjava/SmartCV/BoilerController.java
+++ smartcv/src/main/java/com/teamjava/SmartCV/BoilerController.java
@@ -1,9 +1,18 @@
 package com.teamjava.SmartCV;
 
+import java.io.IOException;
+
 public class BoilerController {
-    private BoilerDTO boilerDTO;
-    private String boilerOutputString;
-    private BoilerReader boilerReader;
-    private BoilerWriter boilerWriter;
-    private BoilerConverter boilerConverter;
+    private static BoilerDTO boilerDTO;
+    private static String boilerOutputString;
+    private static BoilerReader boilerReader;
+    private static BoilerWriter boilerWriter;
+    private static BoilerConverter boilerConverter;
+
+    public static void main(String[] args) throws IOException {
+        String boilerOutputString = "";
+
+        boilerReader = new BoilerReader();
+        boilerOutputString = boilerReader.readFromBoiler(7777);
+    }
 }
--- smartcv/src/main/java/com/teamjava/SmartCV/BoilerReader.java
+++ smartcv/src/main/java/com/teamjava/SmartCV/BoilerReader.java
@@ -1,11 +1,44 @@
 package com.teamjava.SmartCV;
 
+import org.apache.catalina.connector.CoyoteReader;
+
+import java.net.*;
+import java.io.*;
+
 public class BoilerReader {
+    // Variables used by program
     private int port;
     private String outputString;
 
-    public String readFromBoiler(int port) {
+    // Variables used for Sockets
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private OutputStream os;
+    private OutputStreamWriter ows;
+    private BufferedWriter bw;
+    private InputStream is;
+    private InputStreamReader isr;
+
+    public String readFromBoiler(int port) throws IOException {
         String returnString = "";
+
+        clientSocket = new Socket("127.0.0.1", 7777);
+
+
+
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out.println("$CV-CONNECT-$-746069");
+        out.println("$CV-STAT?");
+
+        returnString = in.readLine();
+        System.out.println(returnString);
+        System.out.println(clientSocket.isConnected());
+
+        in.close();
+        out.close();
+        clientSocket.close();
 
         return returnString;
     }
