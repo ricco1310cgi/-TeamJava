0ecb10570b8ec9a4198fb2015f3cd89e79ad84e7
ricco1310cgi <55280954+ricco1310cgi@users.noreply.github.com>
2019-10-08 17:01:54 +0200

    update to the front-end

--- src/main/resources/static/frontend/smartCv.js
+++ src/main/resources/static/frontend/smartCv.js
@@ -1,8 +1,36 @@
+var serverBoilerArray = new Array;
+
+var serverBoiler = {
+    "id": -1,
+    "boilerPressure": -1,
+    "tempInside": -1.1,
+    "tempOutside": -1.1,
+    "timeMovementRecord": -12,
+    "gasUsage": -1.1,
+    "timeRecorder": -10000,
+    "doorClosed": false
+}
+
+//var date = new Date();
+
 function getData(api) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+
+            serverBoilerArray = JSON.parse(this.responseText);           
+            serverBoiler = JSON.parse(this.responseText);
+            document.getElementById("overzichtBinnenTemp").innerHTML = serverBoiler[serverBoilerArray.length - 1].tempInside;
+            document.getElementById("overzichtBoilerDruk").innerHTML = serverBoiler[serverBoilerArray.length - 1].boilerPressure;
+            document.getElementById("overzichtBuitenTemp").innerHTML = serverBoiler[serverBoilerArray.length-1].tempOutside;
+            var date = new Date(serverBoiler[serverBoilerArray.length-1].timeMovementRecord * 1000);
+            document.getElementById("overzichtLaatstThuis").innerHTML = date.toLocaleString();
+            document.getElementById("overzichtGasGebruik").innerHTML = serverBoiler[serverBoilerArray.length-1].gasUsage;
+            document.getElementById("overzichtDeurStatus").innerHTML = serverBoiler[serverBoilerArray.length-1].doorClosed;
+            date = new Date(serverBoiler[serverBoilerArray.length-1].timeRecorder * 1000);
+            document.getElementById("overzichtBoilerTijd").innerHTML = date.toLocaleString();
             document.getElementById("demo").innerHTML = this.responseText;
+            
         }
     };
     xhttp.open("GET", "http://localhost:8082/api/boiler");
@@ -30,6 +58,28 @@ function getAverageTemp(api) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+
+            serverBoilerArray = JSON.parse(this.responseText);           
+            serverBoiler = JSON.parse(this.responseText);
+
+            var barChart = document.getElementById("barChart");
+
+            barChart.width = 300;
+            barChart.height = 400;
+  
+            var ctx = barChart.getContext("2d");
+
+            ctx.clearRect(0, 0, barChart.width, barChart.height);
+
+            for (var i = 0; i < 2; i++) {
+                console.log(serverBoiler[i]);
+                console.log(serverBoiler[i] == typeof Number)
+                    ctx.save();
+                    ctx.fillRect(i * 30, barChart.height, 20, -serverBoiler[i]*10);
+                    ctx.restore();                
+            }
+            
+
             document.getElementById("averageTemp").innerHTML = this.responseText;
         }
     };
@@ -51,4 +101,11 @@ function getTempInside(api) {
 
     xhttp.send();
     console.log('test');
+}
+
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height,color){
+    ctx.save();
+    ctx.fillStyle=color;
+    ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+    ctx.restore();
 }
